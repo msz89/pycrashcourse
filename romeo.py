16be0ef6fcfc4452e7b0c73ce479fc6aa504f938
msz89 <matthewszwec@gmail.com>
2018-08-25 10:35:15 +1000

    dev work

--- romeo.py
+++ romeo.py
@@ -3,16 +3,16 @@ import os
 
 fn = os.path.join("labs", "play.txt")
 print(fn)
-# f = open(fn,"r")
+f = open(fn,"r")
 
-# split_words = f.read().split(" ")
-# # print(split_words)
+split_words = f.read().split(" ")
+# print(split_words)
 
-# ULIST = list()
+ULIST = list()
 
-# for word in split_words:
-# 	if word not in ULIST:
-# 		ULIST.append(word)
+for word in split_words:
+	if word not in ULIST:
+		ULIST.append(word)
 
-# print(ULIST)
-# print(len(ULIST))
+print(ULIST)
+print(len(ULIST))
